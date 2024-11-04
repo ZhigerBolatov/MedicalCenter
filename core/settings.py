7dc53d33d4d075d92a0e786e509fba2ad3c8e29a
Zhiger Bolatov <zhiger.ali@gmail.com>
2024-11-04 15:30:51 +0500

    DRF auth policy update

--- core/settings.py
+++ core/settings.py
@@ -166,6 +166,7 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
